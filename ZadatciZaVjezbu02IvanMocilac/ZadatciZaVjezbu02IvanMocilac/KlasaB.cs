721a14056a14be4b259cda8c83b2e426ab2b1740
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-12-11 17:59:10 +0100

    Add files via upload

---
+++ ZadatciZaVjezbu02IvanMocilac/ZadatciZaVjezbu02IvanMocilac/KlasaB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu02IvanMocilac
+{
+    class KlasaB
+    {
+        public string BezPrvogiZadnjeg(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
---
+++ ZadatciZaVjezbu02IvanMocilac/ZadatciZaVjezbu02IvanMocilac/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatciZaVjezbu02IvanMocilac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Upišite niz znakova: ");
+            string s = Console.ReadLine();
+            KlasaB b = new KlasaB();
+            if(s.Length > 2)
+            {
+                s = b.BezPrvogiZadnjeg(s);
+                Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine("Molimo Vas da upišete više od 2 znaka.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
